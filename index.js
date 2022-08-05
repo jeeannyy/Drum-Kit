d3c7a838d9c1cbdeea5986373fa9e4eed0f5a009
Jeeann <jeeann.kim@gmail.com>
2022-08-05 22:28:10 +0100

    edit js file

--- index.js
+++ index.js
@@ -1,39 +1,81 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-/**
-document.querySelector("button").addEventListener("click", gotClick);
-MDN에 순서랑 자세한거 다 써있으니 새로운 메소드 쓸 땐 귀찮아도 무조건 확인할 것!
-뜻: 첫 번째(all이 아니라서 그런듯) 버튼이라는걸 다큐먼트에서 찾고 이벤트리스너라는걸 그 버튼에 추가한다. 
-'클릭'이라는 말을 들으면 갓클릭이라는 함수를 실행한다.
-여기서 함수 쓸 때 보통 함수 실행 때와 달리 ()를 뒤에 안 쓴다.
-함수()를 쓴다는건 그 즉시 즉시 즉시 아주 바로 실행되니까 못 쓰는거임. 우리는 클릭이라는 일이 일어날 때만 부를거라
-그래서 함수()가 아니라 함수만 쓰는거임!!!!!!!!!!!!!!!! 
+for (var i = 0; i < numberOfDrumButtons; i++) {
 
-function gotClick(){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
-    alert("hey!");
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
+  });
 
 }
 
-**/
+document.addEventListener("keypress", function(event) {
 
-//이걸 이렇게 간단하게 쓸 수도 있음
-//document.querySelector("button").addEventListener("click", function(){alert("hey!");});
+  makeSound(event.key);
 
-alert("hey!");
+  buttonAnimation(event.key);
 
-
-const numberOfDrumButton = document.querySelectorAll(".drum").length;
-
-    for (let i = 0; i < numberOfDrumButton; i++) {
-
-        document.querySelectorAll(".durm")[i].addEventListener("click", function () {
-
-            const audio = new Audio("sounds/crash.mp3");
-            audio.play();
+});
 
 
-    });
+function makeSound(key) {
 
-    }
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
 
-    //*************const = i라고 쓰면 안되고 let은 됨 왜????????????
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+
+
+    default: console.log(key);
+
+  }
+}
+
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+}
